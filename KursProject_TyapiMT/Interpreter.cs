57c3e35949ccc9b93ffe385cb61700da5ac5bb65
sergeyosin <sereosi2017@yandex.ru>
2025-11-09 20:15:48 +0300

    тесты пройдены

--- KursProject_TyapiMT/Interpreter.cs
+++ KursProject_TyapiMT/Interpreter.cs
@@ -2,5 +2,15 @@ namespace KursProject_TyapiMT;
 
 public class Interpreter
 {
+    public static void Start()
+    {
+        List<string> code = File.ReadAllLines("code.txt").ToList();
+        LexicalAnalyzator lx = new LexicalAnalyzator(code);
+        if (lx.Analyze() && SyntaxAnalyzer.Analyze(lx.Tokens) && SemanticAnalyzer.Analyze(lx.Tokens))
+        {
+            Console.Write("Анализаторы выполнены без ошибок");
+        }
+        
+    }
     
 }
